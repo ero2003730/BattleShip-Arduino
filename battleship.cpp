82a685e57a392c5c73d134d2bcbb61713a1bdce2
ero2003730 <96956416+ero2003730@users.noreply.github.com>
2024-04-03 21:25:24 -0300

    Alterei

--- battleship.cpp
+++ battleship.cpp
@@ -10,9 +10,11 @@ enum EstadoInterface
   InterfaceInicial,
   InterfaceDeSelecao,
   InterfaceDePosicaoDosNavios,
-  ComecarJogo,
+  ArduinoEscolhe,
   Torpedo,
-  Submarino
+  Submarino,
+  ComecarJogo,
+  JogoFinalizado
 };
 
 EstadoInterface State = InterfaceInicial;
@@ -25,6 +27,15 @@ enum EstadoTorpedo
   y2,
   Verificacao
 };
+
+enum EstadoAtaque
+{
+  x,
+  y,
+  Ataq
+};
+
+EstadoAtaque StateAtaque = x;
 
 int x1T;
 int y1T;
@@ -64,10 +75,57 @@ bool submarino = false;
 
 EstadoSubmarino StateSubmarino = x1Su;
 
+enum EstadoJogar
+{
+  Ataque,
+  Defesa,
+};
+
+EstadoJogar StateJogar = Ataque;
+
+struct Atacar
+{
+  int x;
+  int y;
+};
+
+Atacar ataquesErrados[20];
+int totalAtaquesErrados = 0;
+Atacar ataquesCertos[5];
+int totalAtaquesCertos = 0;
+
+bool mensagemXAMostrada = false;
+bool mensagemYAMostrada = false;
+
+int xA = -1;
+int yA = -1;
+
+struct Defender
+{
+  int x;
+  int y;
+};
+
+Defender defesaErrados[20];
+int totaldefesaErrados = 0;
+Defender defesaCertos[5];
+int totaldefesaCertos = 0;
+
+// Coordenadas do Torpedeiro
+int x1Ti, y1Ti, x2Ti, y2Ti;
+
+// Coordenadas do Submarino
+int x1Si, y1Si, x2Si, y2Si, x3Si, y3Si;
+
+// Flag para assegurar a criação única do cenário
+bool cenarioCriado = false;
+
 // Variáveis de debounce para cada botão
 unsigned long lastDebounceTime1 = 0;
 unsigned long lastDebounceTime2 = 0;
 unsigned long lastDebounceTime3 = 0;
+unsigned long previousMillis = 0;
+unsigned long previousMillisTimer = 0;
 const long debounceDelay = 50; // debounce delay de 50 milissegundos
 
 int buttonState1 = LOW;
@@ -94,6 +152,7 @@ void setup()
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(buttonPin3, INPUT);
+  randomSeed(analogRead(0));
   Serial.begin(9600);
 }
 
@@ -164,7 +223,7 @@ void loop()
     {
       if (torpedo && submarino)
       {
-        State = ComecarJogo;
+        State = ArduinoEscolhe;
         mensagemMostrada = false;
       }
       else if (torpedo && !submarino)
@@ -256,6 +315,7 @@ void loop()
         // Reseta a flag para permitir que a próxima mensagem seja mostrada
         mensagemY1Mostrada = false;
       }
+      Serial.flush();
     }
     break;
 
@@ -333,6 +393,7 @@ void loop()
       else
       {
         Serial.println("Pontos adicionados com sucesso.");
+        button1 = 0;
         torpedo = true;
         State = InterfaceDePosicaoDosNavios;
         StateTorpedo = x1;
@@ -344,6 +405,9 @@ void loop()
       }
     }
     break;
+
+    default:
+      break;
     }
   }
   break;
@@ -475,12 +539,422 @@ void loop()
         mensagemX3SMostrada = false;
         mensagemY3SMostrada = false;
         mensagemMostrada = false;
+        button1 = 0;
       }
     }
     break;
+
+    default:
+      break;
     }
   }
   break;
+
+  case ArduinoEscolhe:
+  {
+    // Geração aleatória do Torpedeiro
+    x1Ti = random(0, 4); // Gera X inicial (0 a 4)
+    y1Ti = random(0, 4); // Gera Y inicial (0 a 4)
+
+    // Escolha a direção do torpedeiro (horizontal ou vertical)
+    if (random(0, 2) == 0)
+    { // Horizontal
+      x2Ti = x1Ti + 1;
+      y2Ti = y1Ti;
+    }
+    else
+    { // Vertical
+      x2Ti = x1Ti;
+      y2Ti = y1Ti + 1;
+    }
+
+    bool posicaoValida; // Indica se a posição gerada é válida
+    do
+    {
+      posicaoValida = true; // Assume inicialmente que a posição é válida
+
+      x1Si = random(0, 4); // Gera X inicial para o submarino
+      y1Si = random(0, 4); // Gera Y inicial para o submarino
+
+      // Decide a direção do submarino (0 para horizontal, 1 para vertical)
+      int direcao = random(0, 2);
+
+      if (direcao == 0)
+      {                  // Horizontal
+        x2Si = x1Si + 1; // Ponto médio
+        x3Si = x1Si + 2; // Ponto final
+        y2Si = y3Si = y1Si;
+      }
+      else
+      {                  // Vertical
+        y2Si = y1Si + 1; // Ponto médio
+        y3Si = y1Si + 2; // Ponto final
+        x2Si = x3Si = x1Si;
+      }
+
+      // Verifica se o submarino está dentro do grid
+      if (x3Si > 4 || y3Si > 4)
+      {
+        posicaoValida = false;
+      }
+
+      // Verifica se alguma posição do submarino coincide com as do torpedeiro
+      if ((x1Si == x1Ti && y1Si == y1Ti) || (x1Si == x2Ti && y1Si == y2Ti) ||
+          (x2Si == x1Ti && y2Si == y1Ti) || (x2Si == x2Ti && y2Si == y2Ti) ||
+          (x3Si == x1Ti && y3Si == y1Ti) || (x3Si == x2Ti && y3Si == y2Ti))
+      {
+        posicaoValida = false;
+      }
+
+    } while (!posicaoValida); // Repete até que uma posição válida seja encontrada
+
+    cenarioCriado = true; // Marca que o cenário foi criado
+
+    if (!mensagemMostrada)
+    {
+      Serial.print("Posição inicial do Torpedeiro (X1, Y1): (");
+      Serial.print(x1Ti);
+      Serial.print(", ");
+      Serial.print(y1Ti);
+      Serial.println(")");
+
+      Serial.print("Posição final do Torpedeiro (X2, Y2): (");
+      Serial.print(x2Ti);
+      Serial.print(", ");
+      Serial.print(y2Ti);
+      Serial.println(")");
+
+      Serial.print("Posição inicial do Submarino (X1, Y1): (");
+      Serial.print(x1Si);
+      Serial.print(", ");
+      Serial.print(y1Si);
+      Serial.println(")");
+
+      Serial.print("Posições média e final do Submarino (X2, Y2), (X3, Y3): (");
+      Serial.print(x2Si);
+      Serial.print(", ");
+      Serial.print(y2Si);
+      Serial.print(") e (");
+      Serial.print(x3Si);
+      Serial.print(", ");
+      Serial.print(y3Si);
+      Serial.println(")");
+      mensagemMostrada = true;
+    }
+
+    State = ComecarJogo;
+    button1 = 0;
+    button2 = 0;
+    button3 = 0;
+
+  }
+  break;
+
+  case ComecarJogo:
+  {
+    switch (StateJogar)
+    {
+    case Ataque:
+    {
+      previousMillis = millis();
+
+      if ((millis() - previousMillis) <= 30000)
+      {
+        digitalWrite(ledPin1, HIGH);
+        digitalWrite(ledPin2, LOW);
+        digitalWrite(ledPin3, LOW);
+
+        if (button1 == 1 || button2 == 1)
+        {
+          if (totalAtaquesCertos == 0)
+            Serial.println("O jogador nao acertou nenhuma coordenada");
+          else
+          {
+            Serial.println("Coordenadas corretas:");
+
+            for (int i = 0; i < totalAtaquesCertos; i++)
+            {
+              Serial.print("(");
+              Serial.print(ataquesCertos[i].x);
+              Serial.print(", ");
+              Serial.print(ataquesCertos[i].y);
+              Serial.println(")");
+            }
+          }
+
+          if (totalAtaquesErrados == 0)
+            Serial.println("O jogador nao errou nenhuma coodenada");
+          else
+          {
+            Serial.println("Coordenadas erradas:");
+
+            for (int i = 0; i < totalAtaquesErrados; i++)
+            {
+              Serial.print("(");
+              Serial.print(ataquesErrados[i].x);
+              Serial.print(", ");
+              Serial.print(ataquesErrados[i].y);
+              Serial.println(")");
+            }
+          }
+          button1 = 0;
+          button2 = 0;
+        }
+
+        if (button3 == 1)
+        {
+          if ((millis() - previousMillis) < 3000)
+          {
+            if ((millis() - previousMillisTimer) >= 100)
+            {
+              previousMillisTimer = millis();
+
+              if (digitalRead(ledPin1) == HIGH)
+              {
+                digitalWrite(ledPin1, LOW);
+                digitalWrite(ledPin2, LOW);
+                digitalWrite(ledPin3, LOW);
+              }
+
+              else
+              {
+                digitalWrite(ledPin1, HIGH);
+                digitalWrite(ledPin2, HIGH);
+                digitalWrite(ledPin3, HIGH);
+              }
+            }
+          }
+          else
+          {
+            digitalWrite(ledPin1, LOW);
+            digitalWrite(ledPin2, LOW);
+            digitalWrite(ledPin3, LOW);
+
+            State = JogoFinalizado;
+            button3 = 0;
+          }
+        }
+        bool alvoAcertado = false;
+
+        switch(StateAtaque)
+        {
+          case x:
+          {
+            if (!mensagemXAMostrada)
+            {
+              Serial.println("Por favor, insira a coordenada x do ataque.");
+              mensagemXAMostrada = true;
+            }
+            if (Serial.available() > 0)
+            {
+              xA = Serial.parseInt();
+              Serial.println(xA);
+              StateAtaque = y;
+            }
+          }
+          break;
+
+          case y:
+          {
+            if (!mensagemYAMostrada)
+            {
+              Serial.println("Por favor, insira a coordenada y do ataque.");
+              mensagemYAMostrada = true;
+            }
+            if (Serial.available() > 0)
+            {
+              yA = Serial.parseInt();
+              Serial.println(yA);
+              StateAtaque = Ataq;
+            }
+          }
+          break;
+
+          case Ataq:
+          {
+          // Verificação de coordenada já utilizada
+            bool coordenadaJaUtilizada = false;
+            for (int i = 0; i < totalAtaquesCertos; i++)
+            {
+              if (ataquesCertos[i].x == xA && ataquesCertos[i].y == yA)
+              {
+                coordenadaJaUtilizada = true;
+                break;
+              }
+            }
+            for (int i = 0; i < totalAtaquesErrados; i++)
+            {
+              if (ataquesErrados[i].x == xA && ataquesErrados[i].y == yA)
+              {
+                coordenadaJaUtilizada = true;
+                break;
+              }
+            }
+
+            if (coordenadaJaUtilizada)
+            {
+              // Reseta xA e yA após o ataque para evitar reprocessamento
+              xA = -1;
+              yA = -1;
+              Serial.println("Você já atacou essa coordenada. Por favor, escolha outra.");
+            }
+            else
+            { // Verifica se acertou o torpedeiro inimigo
+              if ((xA == x1Ti && yA == y1Ti) || (xA == x2Ti && yA == y2Ti))
+                alvoAcertado = true;
+
+              // Verifica se acertou o submarino inimigo
+              if ((xA == x1Si && yA == y1Si) || (xA == x2Si && yA == y2Si) || (xA == x3Si && yA == y3Si))
+                alvoAcertado = true;
+
+              if (alvoAcertado)
+              {
+                Serial.println("Alvo localizado!");
+                ataquesCertos[totalAtaquesCertos].x = xA;
+                ataquesCertos[totalAtaquesCertos].y = yA;
+                totalAtaquesCertos++;
+              }
+              else
+              {
+                Serial.println("Alvo perdido!");
+                ataquesErrados[totalAtaquesErrados].x = xA;
+                ataquesErrados[totalAtaquesErrados].y = yA;
+                totalAtaquesErrados++;
+              }
+
+              if (totalAtaquesCertos == 5)
+              {
+                digitalWrite(ledPin1, HIGH);
+                digitalWrite(ledPin2, HIGH);
+                digitalWrite(ledPin3, HIGH);
+                Serial.println("Parabens voce ganhou!");
+                State = JogoFinalizado;
+              }
+              else
+              {
+                if (xA != -1 && yA != -1)
+                {
+                  StateJogar = Defesa;
+                  // Reseta xA e yA após o ataque para evitar reprocessamento
+                  xA = -1;
+                  yA = -1;
+                }
+              }
+            }
+          }
+          
+          previousMillis = millis();
+              }
+        }
+
+        
+
+        
+
+        
+    }
+    break;
+
+    case Defesa:
+    {
+      bool coordenadaJaUtilizada;
+      Defender coordenadaDefesa;
+
+      do
+      {
+        coordenadaJaUtilizada = false;
+        coordenadaDefesa.x = random(0, 5); // Gera um número entre 0 e 4
+        coordenadaDefesa.y = random(0, 5); // Gera um número entre 0 e 4
+
+        // Verifica se a coordenada já foi utilizada nos ataques certos
+        for (int i = 0; i < totaldefesaCertos; i++)
+        {
+          if (defesaCertos[i].x == coordenadaDefesa.x && defesaCertos[i].y == coordenadaDefesa.y)
+          {
+            coordenadaJaUtilizada = true;
+            break;
+          }
+        }
+
+        // Verifica se a coordenada já foi utilizada nos ataques errados, se ainda não foi marcada como utilizada
+        if (!coordenadaJaUtilizada)
+        {
+          for (int i = 0; i < totaldefesaErrados; i++)
+          {
+            if (defesaErrados[i].x == coordenadaDefesa.x && defesaErrados[i].y == coordenadaDefesa.y)
+            {
+              coordenadaJaUtilizada = true;
+              break;
+            }
+          }
+        }
+      } while (coordenadaJaUtilizada); // Repete até encontrar uma coordenada não utilizada
+
+      bool defesaAcertou = false; // Inicializa como falso
+
+      // Verifica se acertou o torpedeiro
+      if ((coordenadaDefesa.x == x1T && coordenadaDefesa.y == y1T) || (coordenadaDefesa.x == x2T && coordenadaDefesa.y == y2T))
+      {
+        defesaAcertou = true;
+      }
+
+      // Verifica se acertou o submarino
+      if (!defesaAcertou)
+      { // Só verifica se ainda não acertou o torpedeiro
+        if ((coordenadaDefesa.x == x1S && coordenadaDefesa.y == y1S) ||
+            (coordenadaDefesa.x == x2S && coordenadaDefesa.y == y2S) ||
+            (coordenadaDefesa.x == x3S && coordenadaDefesa.y == y3S))
+        {
+          defesaAcertou = true;
+        }
+      }
+
+      // Antes de verificar o acerto, imprime a coordenada escolhida pela defesa
+      Serial.print("Coordenada da defesa: (");
+      Serial.print(coordenadaDefesa.x);
+      Serial.print(", ");
+      Serial.print(coordenadaDefesa.y);
+      Serial.println(")");
+
+      // Após verificar, registra o resultado conforme acerto ou erro
+      if (defesaAcertou)
+      {
+        defesaCertos[totaldefesaCertos++] = coordenadaDefesa; // Armazena a defesa correta
+        Serial.println("Defesa acertou!");
+      }
+      else
+      {
+        defesaErrados[totaldefesaErrados++] = coordenadaDefesa; // Armazena a defesa errada
+        Serial.println("Defesa errou.");
+      }
+
+      // Verifica se o jogo terminou
+      if (totaldefesaCertos == 5)
+      {
+        Serial.println("Jogo encerrado, Arduino ganhou");
+        digitalWrite(ledPin1, HIGH);
+        digitalWrite(ledPin2, HIGH);
+        digitalWrite(ledPin3, HIGH);
+        State = JogoFinalizado;
+      }
+      else
+      {
+        StateJogar = Ataque; // Passa a vez para o jogador
+      }
+    }
+    break;
+
+    default:
+      break;
+    }
+  }
+  break;
+
+  case JogoFinalizado:
+    break;
+
+  default:
+    break;
   }
 }
 
